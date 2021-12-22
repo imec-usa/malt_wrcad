e29c33540234ff13b30213864ff246a28951e3ff
Trent Josephsen <trent.josephsen@imec-int.com>
2021-12-22 12:38:21 -0500

    Generate a correct TOML file (with comments) Also: - remove the parameter defaults from Configuration, since they are error prone - add MALTVERSION string in case we want to change it

--- malt.h
+++ malt.h
@@ -31,8 +31,9 @@ void malt_status(FILE *, const char *, ...);
   }
 
 #define MALTUSAGE "malt [-h] {-d|-m|-t|-2|-y|-o} <circuit_name>[.<ext_1>[.<ext_2>[...]]]\n"
+#define MALTVERSION "3.0"
 #define MALTHELP                                                            \
-  "MALT 3.0\n"                                                              \
+  "MALT " MALTVERSION "\n"                                                  \
   "  Parametric yield optimization utility for use with WRSpice\n"          \
   "SYNOPSIS\n"                                                              \
   "  " MALTUSAGE "OPTIONS\n"                                                \
